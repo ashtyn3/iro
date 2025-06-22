423e823e2f2a2b8bbf9f5208bef70c8d87bb7d38
ashtyn3 <ashtynmorel@gmail.com>
2025-06-22 18:10:40 -0400

    add inventory

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,5 +1,7 @@
 import { For, onMount } from "solid-js";
 import { Engine } from "~/lib";
+// import hand from "~/lib/assets/hand.png";
+// import handR from "~/lib/assets/hand-r.png";
 
 export default function Game({ engine }: { engine: Engine }) {
 	try {
@@ -27,6 +29,22 @@ export default function Game({ engine }: { engine: Engine }) {
 					</div>
 				</div>
 				<div id="gamebox"></div>
+				<div class="flex flex-row gap-2 justify-center m-5">
+					<div class="border-2 border-white">
+						<img
+							src={player().hands.left.sprite[0]}
+							alt={player().hands.left.name}
+							class="w-15 h-15"
+						/>
+					</div>
+					<div class="border-2 border-white">
+						<img
+							src={player().hands.right.sprite[1]}
+							alt={player().hands.right.name}
+							class="w-15 h-15"
+						/>
+					</div>
+				</div>
 			</div>
 		);
 	} catch (error) {
