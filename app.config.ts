aa65d79ddb92c6489f37a6908100a97c4f5073f3
ashtyn3 <ashtynmorel@gmail.com>
2025-07-20 13:43:33 -0400

    remove pwa

--- app.config.ts
+++ app.config.ts
@@ -4,36 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	vite: {
-		plugins: [
-			tailwindcss(),
-			VitePWA({
-				registerType: "autoUpdate",
-				injectRegister: "auto",
-				workbox: {
-					maximumFileSizeToCacheInBytes: 10000000,
-				},
-				manifest: {
-					name: "Iro",
-					short_name: "Iro",
-					description: "Factory",
-					theme_color: "#000000",
-					background_color: "#000000",
-					display: "standalone",
-					icons: [
-						{
-							src: "/favicon.ico",
-							sizes: "16x16",
-							type: "image/x-icon",
-						},
-					],
-				},
-				devOptions: {
-					enabled: process.env.SW_DEV === "true",
-					type: "module",
-					navigateFallback: "index.html",
-				},
-			}),
-		],
+		plugins: [tailwindcss()],
 	},
 	server: {
 		esbuild: {
